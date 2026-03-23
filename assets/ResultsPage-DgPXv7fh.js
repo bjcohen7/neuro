65c3f5e20f16d32d7eea056850ad4cbaa959007c
NeuroDash Deploy <deploy@myneurotest.com>
2026-03-22 21:09:08 -0400

    fix: add AbortSignal timeouts to prevent infinite loading screen
    - callTrpc now accepts a timeoutMs param (default 26s) using AbortSignal.timeout()
    - ingest.scores and ingest.session calls use 5s timeout each
    - results.submit uses 25s timeout
    - Prevents users getting stuck on the analyzing screen indefinitely
    - 28s outer Promise.race still acts as final safety net

--- assets/ResultsPage-DgPXv7fh.js
+++ assets/ResultsPage-DgPXv7fh.js
@@ -1,4 +1,4 @@
-import{p as Hr,j as l,F as Ds}from"./index-DEjxd3Fo.js";import{b as Ls,a as O,u as Ss}from"./vendor-router-DJxaUcYL.js";import{c as ce}from"./trpc-DchLoi05.js";import"./vendor-react-CNoi_Alz.js";/*!
+import{p as Hr,j as l,F as Ds}from"./index-8NIemz9s.js";import{b as Ls,a as O,u as Ss}from"./vendor-router-DJxaUcYL.js";import{c as ce}from"./trpc-CaVAfbSV.js";import"./vendor-react-CNoi_Alz.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
